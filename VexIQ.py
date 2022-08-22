6c0d8377e41fdf224e218778f49fab81b96cba80
Blaziumm <111737350+Blaziumm@users.noreply.github.com>
2022-08-22 04:49:38 -0400

    Add files via upload
    Python File for a test piece of code written in python 3.7.3

---
+++ VexIQ.py
@@ -0,0 +1,38 @@
+#Vex Omni Wheel Slope Test
+#Made by Evan Wirzburger
+#Team 21037A
+
+#Cords Set Up
+#This is your current Coords
+firstx = 0
+firsty = 0
+#This is the Coords you are going to (Turning into a list)
+Secondx = 1
+Secondy = 1
+#Obsticals (Turning into a list)
+obx = 1
+oby = 1
+
+#Slope Calculations
+changeinx = firstx - Secondx
+changeiny = firsty - Secondy
+slope = changeiny//changeinx
+
+#Loop Prep
+testingx = changeinx
+repeatamount = Secondx
+
+for i in range (50000):
+    ycord = slope * testingx
+    if ycord == oby and testingx == obx:
+        if testingx >= firstx and testingx <= Secondx:
+            print('True Block')
+        else:
+            print('On slope but not Between Values')
+        testingx = testingx + 0.5
+        i = i + 1
+        break
+    testingx = testingx + 1
+    i = i + 1
+        
+
