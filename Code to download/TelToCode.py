bd1e987ee6e2abbce3e50f30e953724fce6050fb
Evan <eclipsium10@gmail.com>
2022-12-21 22:03:17 -0500

    IMPORTANT UPDATE - LARGEST PUSH
    Changed Update time from Seconds to milliseconds, which should allow a more accurate auto, and removed console clutter. 
    
    Cheers!

--- Code to download/TelToCode.py
+++ Code to download/TelToCode.py
@@ -8,7 +8,6 @@ string = ""
 time = ""
 nexttime = ""
 file1.close()
-print(data)
 def velcheck(i):
     global rvel
     global lvel
@@ -46,50 +45,70 @@ def velcheck(i):
                                 lvel = lvel + string[i+7]
                         
 for i in range(len(data)-1):
-    print(i)
     string = data[i]
     nextline = data[i+1]
     string = string.replace("\n", "")
     nextline = nextline.replace("\n", "")
-    print(string)
     for i in range(len(string)):
         if string[i] == "Y":
-            print("Truethat")
-            
-            if string[i+1].isnumeric():
+        
+             if string[i+1].isnumeric():
                 time = string[i+1]
-                print("erm")
                 
                 if string[i+2].isnumeric():
                     time = time + string[i+2]
                     try:
                         if string[i+3] == ",":
                             velcheck(i+3);
-                            print("here")
+                           
                     except IndexError:
                         print("out of range")
+                    if string[i+3].isnumeric():
+                        time = time + string[i+3]
+                        try:
+                            if string[i+4] == ",":
+                                velcheck(i+4);
+                                
+                        except IndexError:
+                            print("out of range")
+                        if string[i+4].isnumeric():
+                            time = time + string[i+4]
+                            try:
+                                if string[i+5] == ",":
+                                    velcheck(i+5);
+                                   
+                            except IndexError:
+                                print("out of range")
+                            if string[i+5].isnumeric():
+                                time = time + string[i+5]
+                                try:
+                                    if string[i+6] == ",":
+                                        velcheck(i+6);
+                                    
+                                except IndexError:
+                                    print("out of range")
+                        
                 else:
                     if string[i] == ",":
                         velcheck(i+2);
-                        print("also here")
+                        
         try:                
             if nextline[i] == "Y":
-                print("Truethat")
+              
             
                 if nextline[i+1].isnumeric():
                     nexttime = nextline[i+1]
                     print(nexttime)
                     if nextline[i+2].isnumeric():
                         nexttime = nexttime + nextline[i+2]
-                        print("nexttime")
-                        print(nexttime)
-                        print("time")
-                        print(time)
         except IndexError:
             print("error")
 
-
     if rvel == lvel:
+        print("nexttime")
+        print(nexttime)
+        print("time")
+        print(time)
         time = int(nexttime) - int(time)
         print("time")
         print(time)
@@ -99,7 +118,7 @@ for i in range(len(data)-1):
         output = "rmotor.set_velocity(" + rvel + ", PERCENT) \n"
         output2 = "lmotor.set_velocity(" + lvel + ", PERCENT) \n"
         time = int(nexttime) - int(time)
-        output3 = "wait(" + str(time) + ", SECONDS) \n"
+        output3 = "wait(" + str(time) + ", MSEC) \n"
         print(output)
         print(output2)
         print(output3)
@@ -113,3 +132,7 @@ for i in range(len(data)-1):
     
     except NameError:
         pass
+            
+
+
+
