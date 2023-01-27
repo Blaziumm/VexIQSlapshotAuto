a48e1c0a186a4741c18db3d871bd5f5a7b04aa81
Evan <eclipsium10@gmail.com>
2023-01-26 21:05:44 -0500

    Update TelToCode.py

--- Code to download/TelToCode.py
+++ Code to download/TelToCode.py
@@ -47,9 +47,34 @@ def velcheck(i):
 for i in range(len(data)-1):
     string = data[i]
     nextline = data[i+1]
+    #Formating
     string = string.replace("\n", "")
     nextline = nextline.replace("\n", "")
+    string = string.replace(".0", "")
+    nextline = nextline.replace(".0", "")
+    string = string.replace(".1", "")
+    nextline = nextline.replace(".1", "")
+    string = string.replace(".2", "")
+    nextline = nextline.replace(".2", "")
+    string = string.replace(".3", "")
+    nextline = nextline.replace(".3", "")
+    string = string.replace(".4", "")
+    nextline = nextline.replace(".4", "")
+    string = string.replace(".5", "")
+    nextline = nextline.replace(".5", "")
+    string = string.replace(".6", "")
+    nextline = nextline.replace(".6", "")
+    string = string.replace(".7", "")
+    nextline = nextline.replace(".7", "")
+    string = string.replace(".8", "")
+    nextline = nextline.replace(".8", "")
+    string = string.replace(".9", "")
+    nextline = nextline.replace(".9", "")
+    string = string.replace(">", "")
+    nextline = nextline.replace(">", "")
+    #end formating
     for i in range(len(string)):
+      #Time
         if string[i] == "Y":
         
              if string[i+1].isnumeric():
@@ -63,65 +88,104 @@ for i in range(len(data)-1):
                            
                     except IndexError:
                         print("out of range")
-                    if string[i+3].isnumeric():
-                        time = time + string[i+3]
-                        try:
-                            if string[i+4] == ",":
+                    try:
+                        if string[i+3].isnumeric():
+                            time = time + string[i+3]
+                            try:
+                              if string[i+4] == ",":
                                 velcheck(i+4);
                                 
-                        except IndexError:
-                            print("out of range")
-                        if string[i+4].isnumeric():
-                            time = time + string[i+4]
-                            try:
-                                if string[i+5] == ",":
-                                    velcheck(i+5);
-                                   
                             except IndexError:
                                 print("out of range")
-                            if string[i+5].isnumeric():
-                                time = time + string[i+5]
+                            if string[i+4].isnumeric():
+                                time = time + string[i+4]
                                 try:
-                                    if string[i+6] == ",":
-                                        velcheck(i+6);
-                                    
+                                  if string[i+5] == ",":
+                                    velcheck(i+5);
+                                   
                                 except IndexError:
                                     print("out of range")
-                        
+                                if string[i+5].isnumeric():
+                                    time = time + string[i+5]
+                                    try:
+                                      if string[i+6] == ",":
+                                            velcheck(i+6);
+                                    
+                                    except IndexError:
+                                        print("out of range")
+                    except IndexError:
+                      print("out of range")
                 else:
                     if string[i] == ",":
                         velcheck(i+2);
-                        
+
+  #Nextline Start              
         try:                
             if nextline[i] == "Y":
               
             
-                if nextline[i+1].isnumeric():
-                    nexttime = nextline[i+1]
-                    print(nexttime)
-                    if nextline[i+2].isnumeric():
-                        nexttime = nexttime + nextline[i+2]
+              if nextline[i+1].isnumeric():
+                nexttime = nextline[i+1]
+                
+                if nextline[i+2].isnumeric():
+                    nexttime = nexttime + nextline[i+2]
+                    try:
+                        if string[i+3] == ",":
+                            velcheck(i+3);
+                           
+                    except IndexError:
+                        print("out of range")
+                    try:
+                        if nextline[i+3].isnumeric():
+                            nexttime = nexttime + nextline[i+3]
+                            try:
+                              if string[i+4] == ",":
+                                velcheck(i+4);
+                                
+                            except IndexError:
+                                print("out of range")
+                            if nextline[i+4].isnumeric():
+                                nexttime = nexttime + nextline[i+4]
+                                try:
+                                  if nextline[i+5] == ",":
+                                    velcheck(i+5);
+                                   
+                                except IndexError:
+                                    print("out of range")
+                                if nextline[i+5].isnumeric():
+                                    nexttime = nexttime + nextline[i+5]
+                                    try:
+                                      if nextline[i+6] == ",":
+                                            velcheck(i+6);
+                                    
+                                    except IndexError:
+                                        print("out of range")
+                    except IndexError:
+                      print("out of range")
         except IndexError:
             print("error")
 
-    if rvel == lvel:
-        print("nexttime")
-        print(nexttime)
-        print("time")
-        print(time)
-        time = int(nexttime) - int(time)
-        print("time")
-        print(time)
-        output = "fowardvel(" + str(rvel)+ "," + str(time) + ")\n"
-        print(output)
-    else:
-        output = "rmotor.set_velocity(" + rvel + ", PERCENT) \n"
-        output2 = "lmotor.set_velocity(" + lvel + ", PERCENT) \n"
-        time = int(nexttime) - int(time)
-        output3 = "wait(" + str(time) + ", MSEC) \n"
-        print(output)
-        print(output2)
-        print(output3)
+        if i == len(data) - 1:
+          print("nexttime")
+          print(nexttime)
+          print("time")
+          print(time)
+          output = "rmotor.set_velocity(" + rvel + ", PERCENT) \n"
+          output2 = "lmotor.set_velocity(" + lvel + ", PERCENT) \n"
+          print(output)
+          print(output2)
+        else:
+          print("nexttime")
+          print(nexttime)
+          print("time")
+          print(time)
+          output = "rmotor.set_velocity(" + rvel + ", PERCENT) \n"
+          output2 = "lmotor.set_velocity(" + lvel + ", PERCENT) \n"
+          newtime = int(nexttime) - int(time)
+          output3 = "wait(" + str(newtime) + ", MSEC) \n"
+          print(output)
+          print(output2)
+          print(output3)
 
     file1 = open("code.txt", "a")
     file1.write(output)
@@ -132,7 +196,3 @@ for i in range(len(data)-1):
     
     except NameError:
         pass
-            
-
-
-
